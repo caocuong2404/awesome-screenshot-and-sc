5903bb097d57d9221601d1896b16279bd258b590
Cuong C. Nguyen <cuongnc2404@gmail.com>
2022-08-08 15:26:36 +0700

    v4.3.6

---
+++ javascripts/notification.js
@@ -0,0 +1,1 @@
+var Bg=chrome.extension.getBackgroundPage(),time=60,interval=null;function stop(){Bg.stopStream(),window.close()}interval=setInterval((function(){time--,document.querySelector("#timer").innerText="00:"+time,0===time&&stop()}),1e3),document.querySelector("#btn").addEventListener("click",(function(){clearInterval(interval),interval=null,window.close()}),!1);
